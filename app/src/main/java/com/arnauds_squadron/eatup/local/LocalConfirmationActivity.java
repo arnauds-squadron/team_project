9a22da104368dde0b6dd5555c8d11b4b4f118834
carolyn-wong <carolynwong@fb.com>
2019-07-16 15:51:23 -0700

    added local confirmation to locals directory

--- app/src/main/java/com/arnauds_squadron/eatup/local/LocalConfirmationActivity.java
+++ app/src/main/java/com/arnauds_squadron/eatup/local/LocalConfirmationActivity.java
@@ -1,9 +1,11 @@
-package com.arnauds_squadron.eatup;
+package com.arnauds_squadron.eatup.local;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.arnauds_squadron.eatup.R;
 
 import butterknife.BindView;
 
