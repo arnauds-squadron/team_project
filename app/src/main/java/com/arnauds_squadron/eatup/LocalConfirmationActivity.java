35558f79963b0414ac0b4078520fd64ce77c55f0
carolyn-wong <carolynwong@fb.com>
2019-07-16 15:49:27 -0700

    added local confirmation activity screen

---
+++ app/src/main/java/com/arnauds_squadron/eatup/LocalConfirmationActivity.java
@@ -0,0 +1,36 @@
+package com.arnauds_squadron.eatup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+
+public class LocalConfirmationActivity extends AppCompatActivity {
+
+    @BindView(R.id.btAccept)
+    Button btAccept;
+    @BindView(R.id.btAccept)
+    Button btDeny;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local_confirmation);
+
+        btAccept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO add database update action
+            }
+        });
+
+        btDeny.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO add database update action
+            }
+        });
+    }
+}
