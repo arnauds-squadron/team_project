4ddac1626f9550aa6cedfa063618d41fe7f150cd
Victor Araujo <varaujo@fb.com>
2019-07-16 15:37:19 -0700

    refactored local into new local package, and removed unnecessary vars from pager adapter

--- app/src/main/java/com/arnauds_squadron/eatup/local/LocalFragment.java
+++ app/src/main/java/com/arnauds_squadron/eatup/local/LocalFragment.java
@@ -1,18 +1,18 @@
-package com.arnauds_squadron.eatup;
+package com.arnauds_squadron.eatup.local;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.arnauds_squadron.eatup.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -35,13 +35,6 @@ public class LocalFragment extends Fragment {
 
     @BindView(R.id.etEventTime)
     EditText etEventTime;
-
-    @BindView(R.id.btnConfirm)
-    Button btnConfirm;
-
-    @Nullable
-    @BindView(R.id.sliding_tabs)
-    TabLayout tlTabs;
 
     private Activity activity;
 
