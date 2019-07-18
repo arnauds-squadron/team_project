5a9e32aaf9959141af662f336bd7116b719b7811
Victor Araujo <varaujo@fb.com>
2019-07-18 11:38:09 -0700

    updated model to use geopoint for the address fieled

---
+++ app/src/main/java/com/arnauds_squadron/eatup/local/creation/AddressFragment.java
@@ -0,0 +1,88 @@
+package com.arnauds_squadron.eatup.local.creation;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.arnauds_squadron.eatup.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AddressFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AddressFragment extends Fragment {
+
+    private final static String TAG = "LocalFragment";
+
+    @BindView(R.id.etEventAddress)
+    EditText etEventAddress;
+
+    @BindView(R.id.etEventFoodType)
+    EditText etEventFoodType;
+
+    @BindView(R.id.etEventTime)
+    EditText etEventTime;
+
+    private Activity activity;
+
+    public static AddressFragment newInstance() {
+        Bundle args = new Bundle();
+        AddressFragment fragment = new AddressFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_local, container, false);
+        // Binds the views with butterknife
+        ButterKnife.bind(this, view);
+        activity = getActivity();
+        return view;
+    }
+
+    @OnClick(R.id.btnConfirm)
+    public void confirmEvent() {
+        String address = etEventAddress.getText().toString();
+        String food = etEventFoodType.getText().toString();
+        String time = etEventTime.getText().toString();
+
+        // TODO: validate data
+        // TODO: upload event to parse server
+        // TODO: only return to home screen within onsuccess
+
+        Log.i(TAG, "confirming");
+        if(getFragmentManager() != null) {
+            Log.i(TAG, "confirming not null");
+
+            try {
+               TabLayout tabLayout = activity.findViewById(R.id.sliding_tabs);
+               tabLayout.getTabAt(1).select();
+               Toast.makeText(activity, "Event created", Toast.LENGTH_SHORT).show();
+            }
+            catch (NullPointerException e) {
+                Log.e(TAG, "Activity, tab layout, or home tab is null");
+                Toast.makeText(activity, "Could not create event", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
