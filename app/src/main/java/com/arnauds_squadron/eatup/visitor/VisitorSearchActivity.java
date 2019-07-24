931b966261ba0bce231243ad6346837b98b24048
carolyn-wong <carolynwong@fb.com>
2019-07-24 13:24:50 -0700

    added search by location using Google Places API

--- app/src/main/java/com/arnauds_squadron/eatup/visitor/VisitorSearchActivity.java
+++ app/src/main/java/com/arnauds_squadron/eatup/visitor/VisitorSearchActivity.java
@@ -20,6 +20,13 @@ import android.widget.Toast;
 import com.arnauds_squadron.eatup.R;
 import com.arnauds_squadron.eatup.models.Event;
 import com.arnauds_squadron.eatup.utils.EndlessRecyclerViewScrollListener;
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseGeoPoint;
@@ -27,6 +34,7 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -51,7 +59,7 @@ public class VisitorSearchActivity extends AppCompatActivity implements AdapterV
     private ParseUser user;
     private final static Double DEFAULT_COORD = 0.0;
     private int searchCategory;
-
+    private int AUTOCOMPLETE_REQUEST_CODE = 17;
 
     @BindView(R.id.rvSearchResults)
     RecyclerView rvEvents;
@@ -87,7 +95,7 @@ public class VisitorSearchActivity extends AppCompatActivity implements AdapterV
         resultsSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextChange(String arg0) {
-                // TODO Auto-generated method stub
+                // don't do anything
                 return false;
             }
 
@@ -104,13 +112,12 @@ public class VisitorSearchActivity extends AppCompatActivity implements AdapterV
                     resultsSearchView.clearFocus();
                     resultsSearchView.setQuery(query, false);
                     return true;
-                    // TODO figure out how to manipulate soft input keyboard state for easier user input
                 }
                 else {
                     // prevent submission if no category selected
                     resultsSearchView.setQuery(query, false);
                     Toast.makeText(getApplicationContext(), "Select a search category.", Toast.LENGTH_SHORT).show();
-                    return false;
+                    return true;
                 }
             }
         });
@@ -130,8 +137,7 @@ public class VisitorSearchActivity extends AppCompatActivity implements AdapterV
             String query = intent.getStringExtra(SearchManager.QUERY);
             Toast.makeText(this, query, Toast.LENGTH_SHORT).show();
             int newSearchCategory = intent.getIntExtra(SEARCH_CATEGORY, 0);
-            searchSpinner.setSelection(newSearchCategory);
-            // TODO find out how to stop the keyboard from popping up, keep search term in the search bar
+            // TODO keep search term in the search bar
             Log.d("VisitorSearchActivity", "spinner position: " + newSearchCategory);
             switch(newSearchCategory) {
                 case USER_SEARCH:
@@ -141,24 +147,23 @@ public class VisitorSearchActivity extends AppCompatActivity implements AdapterV
                     loadTopEvents(query);
                     // TODO get search suggestions of cuisines from the Yelp Search API
                     break;
-                case LOCATION_SEARCH:
-                    Log.d("VisitorSearchActivity", "location search:" + query);
-                    //locationSearch(address);
-                    // TODO location searches - use the location bar in the events creation screen?
-                    break;
             }
         }
         // otherwise called by a click on something in VisitorFragment
         else {
             searchCategory = intent.getIntExtra(SEARCH_CATEGORY, 0);
-            // display user's choice in the spinner
-            searchSpinner.setSelection(searchCategory);
             // if not any of the categories, user clicked on current/previous location
-            if(searchCategory == 0) {
+            if (searchCategory == NO_SEARCH) {
                 Double latitude = intent.getDoubleExtra("latitude", DEFAULT_COORD);
                 Double longitude = intent.getDoubleExtra("longitude", DEFAULT_COORD);
                 ParseGeoPoint location = new ParseGeoPoint(latitude, longitude);
                 locationSearch(location);
+            } else if (searchCategory == LOCATION_SEARCH) {
+                startLocationSearchActivity();
+            }
+            else {
+                // display user's choice in the spinner
+                searchSpinner.setSelection(searchCategory);
             }
         }
 
@@ -176,7 +181,6 @@ public class VisitorSearchActivity extends AppCompatActivity implements AdapterV
         // add endless scroll listener to RecyclerView and load items
         rvEvents.addOnScrollListener(scrollListener);
 
-        // TODO add the sort functionality by distance (can later add more "sort by criteria")
         // loadTopEvents(new Date(0));
 
         // set up refresh listener that triggers new data loading
@@ -197,9 +201,11 @@ public class VisitorSearchActivity extends AppCompatActivity implements AdapterV
 
 
     // methods for the search category spinner
-    public void onItemSelected(AdapterView<?> parent, View view,
-                               int pos, long id) {
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
         searchCategory = pos;
+        if(searchCategory == LOCATION_SEARCH) {
+            startLocationSearchActivity();
+        }
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
@@ -228,19 +234,55 @@ public class VisitorSearchActivity extends AppCompatActivity implements AdapterV
         });
     }
 
+    // handle results from a location search
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = Autocomplete.getPlaceFromIntent(data);
+                Double latitude = place.getLatLng().latitude;
+                Double longitude = place.getLatLng().longitude;
+                ParseGeoPoint location = new ParseGeoPoint(latitude, longitude);
+                locationSearch(location);
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+                Toast.makeText(this, "Location search error. Try again.", Toast.LENGTH_SHORT).show();
+                Status status = Autocomplete.getStatusFromIntent(data);
+                Log.i("VisitorSearchActivity", status.getStatusMessage());
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+                Toast.makeText(this, "Location search canceled", Toast.LENGTH_SHORT).show();
+            }
+            searchSpinner.setSelection(NO_SEARCH);
+        }
+    }
+
+    private void startLocationSearchActivity() {
+        // for location searches, return latlng place data after user makes a selection
+        List<Place.Field> fields = Arrays.asList(Place.Field.LAT_LNG);
+        // Start the location autocomplete intent.
+        Intent locationIntent = new Autocomplete.IntentBuilder(
+                AutocompleteActivityMode.FULLSCREEN, fields)
+                .build(this);
+        startActivityForResult(locationIntent, AUTOCOMPLETE_REQUEST_CODE);
+    }
+
     private void locationSearch(ParseGeoPoint geoPoint) {
         final Event.Query eventsQuery = new Event.Query();
         eventsQuery.getClosest(geoPoint).getTop().withHost();
-
         eventsQuery.findInBackground(new FindCallback<Event>() {
             @Override
             public void done(List<Event> objects, ParseException e) {
                 if (e == null) {
-                    for (int i = 0; i < objects.size(); ++i) {
-                        mEvents.add(objects.get(i));
-                        eventAdapter.notifyItemInserted(mEvents.size() - 1);
-                        // on successful reload, signal that refresh has completed
-                        // swipeContainer.setRefreshing(false);
+                    if(objects.size() != 0) {
+                        for (int i = 0; i < objects.size(); ++i) {
+                            mEvents.add(objects.get(i));
+                            eventAdapter.notifyItemInserted(mEvents.size() - 1);
+                            // on successful reload, signal that refresh has completed
+                            // swipeContainer.setRefreshing(false);
+                        }
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "No events found.", Toast.LENGTH_SHORT).show();
                     }
                 } else {
                     e.printStackTrace();
@@ -259,9 +301,16 @@ public class VisitorSearchActivity extends AppCompatActivity implements AdapterV
             @Override
             public void done(List<Event> objects, ParseException e) {
                 if (e == null) {
-                    for (int i = 0; i < objects.size(); ++i) {
-                        mEvents.add(objects.get(i));
-                        eventAdapter.notifyItemInserted(mEvents.size() - 1);
+                    if(objects.size() != 0) {
+                        for (int i = 0; i < objects.size(); ++i) {
+                            mEvents.add(objects.get(i));
+                            eventAdapter.notifyItemInserted(mEvents.size() - 1);
+                            // on successful reload, signal that refresh has completed
+                            // swipeContainer.setRefreshing(false);
+                        }
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "No events found.", Toast.LENGTH_SHORT).show();
                     }
                 } else {
                     e.printStackTrace();
@@ -279,9 +328,16 @@ public class VisitorSearchActivity extends AppCompatActivity implements AdapterV
             @Override
             public void done(List<Event> objects, ParseException e) {
                 if (e == null) {
-                    for (int i = 0; i < objects.size(); ++i) {
-                        mEvents.add(objects.get(i));
-                        eventAdapter.notifyItemInserted(mEvents.size() - 1);
+                    if(objects.size() != 0) {
+                        for (int i = 0; i < objects.size(); ++i) {
+                            mEvents.add(objects.get(i));
+                            eventAdapter.notifyItemInserted(mEvents.size() - 1);
+                            // on successful reload, signal that refresh has completed
+                            // swipeContainer.setRefreshing(false);
+                        }
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "No events found.", Toast.LENGTH_SHORT).show();
                     }
                 } else {
                     e.printStackTrace();
