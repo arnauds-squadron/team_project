f8c137c12a7b2c22b1b9aa7d7117b0f17d12b0ab
Victor Araujo <varaujo@fb.com>
2019-08-05 14:40:13 -0700

    refactoring review functionality to the date fragment

--- app/src/main/java/com/arnauds_squadron/eatup/local/SetupFragmentPagerAdapter.java
+++ app/src/main/java/com/arnauds_squadron/eatup/local/SetupFragmentPagerAdapter.java
@@ -6,7 +6,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 import com.arnauds_squadron.eatup.local.setup.AddressFragment;
 import com.arnauds_squadron.eatup.local.setup.DateFragment;
-import com.arnauds_squadron.eatup.local.setup.ReviewFragment;
 import com.arnauds_squadron.eatup.local.setup.start.StartFragment;
 import com.arnauds_squadron.eatup.local.setup.tags.TagsFragment;
 
@@ -14,9 +13,9 @@ import com.arnauds_squadron.eatup.local.setup.tags.TagsFragment;
  * Pager Adapter to handle all the setup fragments we need to create an event
  */
 public class SetupFragmentPagerAdapter extends FragmentPagerAdapter {
-    private final int PAGE_COUNT = 5;
+    private final int PAGE_COUNT = 4;
 
-    public SetupFragmentPagerAdapter(FragmentManager fm) {
+    SetupFragmentPagerAdapter(FragmentManager fm) {
         super(fm);
     }
 
@@ -30,13 +29,11 @@ public class SetupFragmentPagerAdapter extends FragmentPagerAdapter {
         if(position == 0)
             return new StartFragment();
         if(position == 1)
-            return new TagsFragment();
-        if (position == 2)
             return new AddressFragment();
+        if (position == 2)
+            return new TagsFragment();
         if (position == 3)
             return new DateFragment();
-        if (position == 4)
-            return new ReviewFragment();
         return new StartFragment();
     }
 }
--- app/src/main/java/com/arnauds_squadron/eatup/local/setup/DateFragment.java
+++ app/src/main/java/com/arnauds_squadron/eatup/local/setup/DateFragment.java
@@ -1,37 +1,62 @@
 package com.arnauds_squadron.eatup.local.setup;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.arnauds_squadron.eatup.R;
+import com.arnauds_squadron.eatup.models.Business;
+import com.arnauds_squadron.eatup.models.Event;
 import com.arnauds_squadron.eatup.utils.FormatHelper;
 import com.arnauds_squadron.eatup.utils.UIHelper;
+import com.arnauds_squadron.eatup.yelp_api.YelpApiResponse;
+import com.arnauds_squadron.eatup.yelp_api.YelpData;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import retrofit2.Call;
+import retrofit2.Callback;
 
 /**
  * Fragment that asks the user to input the date and time of their event through a series of
  * input dialogs.
  */
-public class DateFragment extends Fragment {
+public class DateFragment extends Fragment implements OnMapReadyCallback {
+
+    @BindView(R.id.tvAddress)
+    TextView tvAddress;
+
+    @BindView(R.id.tvTags)
+    TextView tvTags;
 
     @BindView(R.id.tvSelectedDate)
     TextView tvSelectedDate;
@@ -45,7 +70,18 @@ public class DateFragment extends Fragment {
     @BindView(R.id.ivClock)
     ImageView clock;
 
+    @BindView(R.id.tvMaxGuests)
+    TextView tvMaxGuests;
+
+    @BindView(R.id.cbOver21)
+    CheckBox cbOver21;
+
+    @BindView(R.id.etEventTitle)
+    EditText etEventTitle;
+
     private OnFragmentInteractionListener mListener;
+
+    private Event event;
     // Calendar object that holds the date and time that the user selects
     private Calendar selectedTime;
     // Booleans to make sure the user selects a time before moving on
@@ -76,14 +112,17 @@ public class DateFragment extends Fragment {
 
         if (isVisibleToUser) {
             UIHelper.hideKeyboard(getActivity(), getView());
-            // delay ui thread to show date after keyboard has disappeared
-            final Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    showDatePickerDialog();
-                }
-            }, 250);
+            event = mListener.getCurrentEvent();
+
+            SupportMapFragment mapFragment = (SupportMapFragment)
+                    getChildFragmentManager().findFragmentById(R.id.mapFragment);
+            mapFragment.getMapAsync(this); // update the map
+
+            if (event.getDate() != null) { // recent event, fill in all the fields
+                setPreviousFields();
+            } else {
+               showDatePickerDialog();
+            }
         }
     }
 
@@ -105,6 +144,51 @@ public class DateFragment extends Fragment {
         } catch (ClassCastException e) {
             throw new ClassCastException(fragment.toString() + " must implement the interface");
         }
+    }
+
+    /**
+     * Communicates with the LocalFragment with the listener to create the event with the
+     * given event name. Requires the name to not be empty or just spaces.
+     */
+    @OnClick(R.id.btnCreateEvent)
+    public void createEvent() {
+        final String eventTitle = etEventTitle.getText().toString().trim();
+
+        if (TextUtils.isEmpty(eventTitle)) {
+            Toast.makeText(getActivity(), "Give your event a name!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Context context = getContext();
+        //call the HomeDetailsActivity.apiAuth to get the Authorization and return a service for the ApiResponse
+        // if we have a response, then get the specific information defined in the Business Class
+        Call<YelpApiResponse> meetUp = YelpData.retrofit(context).getLocation(
+                event.getAddress().getLatitude(), event.getAddress().getLongitude(),
+                event.getTags().get(0), 50);
+
+        meetUp.enqueue(new Callback<YelpApiResponse>() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onResponse(@NonNull Call<YelpApiResponse> call,
+                                   @NonNull retrofit2.Response<YelpApiResponse> response) {
+                if (response.isSuccessful()) {
+                    YelpApiResponse yelpApiResponse = response.body();
+                    if (yelpApiResponse != null && yelpApiResponse.businessList.size() > 0) {
+                        Business restaurant = yelpApiResponse.businessList.get(0);
+                        mListener.createEvent(eventTitle, restaurant.imageUrl);
+                    } else {
+                        mListener.createEvent(eventTitle, null);
+                    }
+                } else {
+                    mListener.createEvent(eventTitle, null);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<YelpApiResponse> call, @NonNull Throwable t) {
+                t.printStackTrace();
+            }
+        });
     }
 
     /**
@@ -144,18 +228,6 @@ public class DateFragment extends Fragment {
     @OnClick(R.id.tvSelectedTime)
     public void updateTime() {
         showTimePickerDialog();
-    }
-
-    /**
-     * Communicates with the LocalFragment to move to the next fragment once the date and time are
-     * set.
-     */
-    @OnClick(R.id.btnNext)
-    public void goToNextFragment() {
-        if (dateSet && timeSet)
-            mListener.updateDate(selectedTime.getTime());
-        else
-            Toast.makeText(getActivity(), "Select a date and time!", Toast.LENGTH_SHORT).show();
     }
 
     /**
@@ -238,11 +310,47 @@ public class DateFragment extends Fragment {
         clock.setVisibility(View.VISIBLE);
     }
 
+    /**
+     * Initialize all the TextViews and details of the recent event
+     */
+    private void setPreviousFields() {
+        tvTags.setText(FormatHelper.listToString(event.getTags()));
+        tvMaxGuests.setText(String.format(Locale.ENGLISH, "%d", event.getMaxGuests()));
+        cbOver21.setChecked(event.getOver21());
+        tvAddress.setText(event.getAddressString());
+        tvSelectedDate.setText(FormatHelper.formatDateWithMonthNames(event.getDate()));
+        tvSelectedTime.setText(FormatHelper.formatTime(event.getDate(), getActivity()));
+    }
+
+    /**
+     * Called whenever getMapAsync() is called, and sets up the map
+     *
+     * @param googleMap The map obtained from the maps API
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng eventLocation = new LatLng(event.getAddress().getLatitude(),
+                event.getAddress().getLongitude());
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(eventLocation,
+                UIHelper.DETAILED_MAP_ZOOM_LEVEL));
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(eventLocation)
+                .title(event.getTitle())
+                .snippet("snippet")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+    }
+
     public interface OnFragmentInteractionListener {
         /**
-         * When called by the parent fragment, it should switch to the next fragment in the
-         * setup queue
+         * Gets the created event from the parent fragment
          */
-        void updateDate(Date date);
+        Event getCurrentEvent();
+
+        /**
+         * Method that triggers the event creation method in the parent fragment
+         */
+        void createEvent(String eventTitle, String imageUrl);
     }
 }
