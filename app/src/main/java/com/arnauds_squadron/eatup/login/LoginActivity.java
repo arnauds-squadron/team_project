04b1c062c57a303f897269b17babfb4cdf6d07b1
Victor Araujo <varaujo@fb.com>
2019-08-09 10:47:57 -0700

    added ripple effect to log in button and error and sucess messages

--- app/src/main/java/com/arnauds_squadron/eatup/login/LoginActivity.java
+++ app/src/main/java/com/arnauds_squadron/eatup/login/LoginActivity.java
@@ -3,12 +3,16 @@ package com.arnauds_squadron.eatup.login;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.arnauds_squadron.eatup.MainActivity;
 import com.arnauds_squadron.eatup.R;
@@ -34,7 +38,11 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.btnSignup)
     Button btnSignup;
 
-    public static String name;
+    @BindView(R.id.tvError)
+    TextView tvError;
+
+    private Handler messageHandler;
+    private Runnable messageRunnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,61 +51,104 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
+        // Persist user if possible
         ParseUser currentUser = ParseUser.getCurrentUser();
         if (currentUser != null) {
             Constants.CURRENT_USER = currentUser;
             goToMainActivity();
         }
 
+        // Set the hidden password behavior
         etPassword.setTypeface(Typeface.DEFAULT);
         etPassword.setTransformationMethod(new PasswordTransformationMethod());
+        // Clicking on the check mark in the soft keyboard will attempt to log you in
+        etPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    login();
+                    return true;
+                }
+                return false;
+            }
+        });
 
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
-                startActivity(intent);
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
             }
         });
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String username = etUsername.getText().toString();
-                final String password = etPassword.getText().toString();
-                login(username, password);
+                login();
             }
         });
-    }
 
-    private void login(final String username, final String password) {
-        ParseUser.becomeInBackground("session-token-here", new LogInCallback() {
-            public void done(ParseUser user, ParseException e) {
-                if (user == null) {
-                    Log.e("LoginActivity", "Unknown user");
-                }
-            }
-        });
-        ParseUser.logInInBackground(username, password, new LogInCallback() {
+        messageHandler = new Handler();
+        messageRunnable = new Runnable() {
             @Override
-            public void done(ParseUser user, ParseException e) {
-                if (e == null) {
-                    ParseUser currentUser = ParseUser.getCurrentUser();
-                    if (currentUser != null) {
-                        name = username;
-                        user.setUsername(username);
-                        Constants.CURRENT_USER = currentUser;
-                        goToMainActivity();
-                    }
-
-                } else {
-                    Log.e("LoginActivity", "Login failure");
-                    e.printStackTrace();
-                }
+            public void run() {
+                tvError.setAlpha(tvError.getAlpha() - 0.05f);
+                if (tvError.getAlpha() > 0)
+                    messageHandler.postDelayed(this, 20);
             }
-        });
+        };
     }
 
+    /**
+     * Attempts to login the user through the Parse server with the given username and password
+     */
+    private void login() {
+        final String username = etUsername.getText().toString();
+        final String password = etPassword.getText().toString();
+
+        if (username.trim().isEmpty()) {
+            showMessage("Enter a username");
+        } else if (password.trim().isEmpty()) {
+            showMessage("Enter a password");
+        } else {
+            ParseUser.logInInBackground(username, password, new LogInCallback() {
+                @Override
+                public void done(ParseUser user, ParseException e) {
+                    if (e == null) {
+                        ParseUser currentUser = ParseUser.getCurrentUser();
+                        if (currentUser != null) {
+                            showMessage("Logging in...");
+                            user.setUsername(username);
+                            Constants.CURRENT_USER = currentUser;
+                            goToMainActivity();
+                        } else {
+                            showMessage("Something went wrong");
+                        }
+
+                    } else {
+                        showMessage("Error logging in");
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
+    /**
+     * Sets the error TextView to show the text provided, and starts a runnable that slowly fades
+     * the message out
+     *
+     * @param message The message to display
+     */
+    private void showMessage(String message) {
+        tvError.setText(message);
+        tvError.setAlpha(1);
+        messageHandler.removeCallbacks(messageRunnable);
+        messageHandler.postDelayed(messageRunnable, 1000);
+    }
+
+    /**
+     * Goes to the MainActivity if the user is logged in successfully
+     */
     private void goToMainActivity() {
         Log.d("LoginActivity", "Login Successful");
         final Intent intent = new Intent(LoginActivity.this, MainActivity.class);
